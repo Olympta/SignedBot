238eeae76bb768e6ea94a50f2e6dcbea7d2f7393
Monotrix <ajaidan0@gmail.com>
2021-04-12 00:34:46 -0400

    Fix up reboot command some more

--- src/commands/reboot.js
+++ src/commands/reboot.js
@@ -3,11 +3,11 @@ const Discord = require("discord.js");
 module.exports = {
     name: "reboot",
     description: "Reboot Command",
-    execute(client, message, config) {
+    async execute(client, message, config) {
         try {
             if (!config.devs.includes(message.author.id)) return;
-            message.channel.send("Rebooting...")
-            process.exit()
+            await message.channel.send("Rebooting...");
+            process.exit();
         } catch (e) {
             console.log(e)
             client.guilds.cache.get(config.logchannel[0]).channels.cache.get(config.logchannel[1]).send("ERROR with ``" + module.exports.name + "``\n```" + e + "```")
