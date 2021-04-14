eb27fb18ee57c8b696b05707a06b3e8808e1c417
Monotrix <ajaidan0@gmail.com>
2021-04-14 14:58:14 -0400

    fix

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,12 +1,11 @@
-const { Message } = require("eris");
 const db = require("quick.db");
 
 module.exports = async (client, msg) => {
     if (msg.channel.type != 0) return;
-    if (!msg.member) return;
+    if (!msg.member.guild) return;
     if (!db.get(`prefix-${msg.member.guild.id}`)) db.set(`prefix-${msg.member.guild.id}`, client.foundation.config.globalPrefix);
-    if (msg.content.startsWith(db.get(`prefix-${msg.member.guild.id}`)) || msg.content.startsWith(`<@${client.user.id}>`)) {} else return;
-    let msgFiltered = msg.content.toLowerCase().replace(db.get(`prefix-${msg.member.guild.id}`), "").replace("<@" + client.user.id + "> ", "").replace("<@" + client.user.id + ">" , "").replace("<@!" + client.user.id + "> ", "").replace("<@!" + client.user.id + ">" , "");
+    if (msg.content.startsWith(db.get(`prefix-${msg.member.guild.id}`)) || msg.content.startsWith(`<@${client.user.id}>`) || msg.content.startsWith(`<@!${client.user.id}>`)) {} else { return; }
+    let msgFiltered = msg.content.toLowerCase().replace(db.get(`prefix-${msg.member.guild.id}`), "").replace(`<@${client.user.id}> `, "").replace(`<@${client.user.id}>` , "").replace(`<@!${client.user.id}> `, "").replace(`<@!${client.user.id}>` , "");
     let cmd = client.foundation.commands.get(msgFiltered.split(" ")[0])
     if (!cmd) {
         client.foundation.commands.forEach(command => {
